b3ec5d90a23344c42169f8dbc0d8178d48d4b9cd
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-07-25 07:20:39 +0300

    release v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+	root: true,
+	extends: '@react-native-community',
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+				useTabs: true,
+				tabWidth: 2,
+			},
+		],
+	},
 };
